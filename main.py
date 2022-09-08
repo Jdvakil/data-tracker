e74d04876b29d9436ec6931002a99930f0bfb11f
Giri Anantharaman <giriman@fb.com>
2022-09-07 20:21:11 -0400

    draft of total trajs

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+from re import L
+
+PATH = "/mnt/robopen_dataset/robopen_dataset/v0.2"
+
+def get_dir():
+    return next(os.walk(PATH))[1]
+def get_names():
+    names = get_dir()
+    names_stripped = []
+    for i in names:
+        names_stripped.append(i.split("_"))
+    count = 0
+    while count < len(names_stripped):
+        if names_stripped[count][-2:-1] == "data":
+            names_stripped[count].pop()
+        count+=1
+        
+    names_final = []  
+    temp = ""  
+    for i in names_stripped:
+        for j in i:
+            temp+=j + " "
+        names_final.append(temp.rstrip())
+        temp = ""
+    return(names_final)
+
+def main():
+    total = 0
+    ls = get_dir()
+    for i in ls:
+        DIR_PATH = os.path.join(PATH,i)
+        for root, dirs, files in os.walk(DIR_PATH):
+            for j in files:
+                if j.endswith(".pickle"):
+                    #print(j)
+                    total+=1
+
+    print(total * 25)
+
+if __name__ == "__main__":
+    main()
